c3b720a76206007c716ceeeaece38255401171af
Pedro Vinincius <viniciuspedro0905@gmail.com>
2025-04-24 22:57:44 -0300

    :sparkles: feat: Created the Card controller and some Endpoints

---
+++ src/main/java/br/fatec/easycoast/resources/CardController.java
@@ -0,0 +1,59 @@
+package br.fatec.easycoast.resources;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import br.fatec.easycoast.dtos.card.CardRequest;
+import br.fatec.easycoast.dtos.card.CardResponse;
+import br.fatec.easycoast.services.CardService;
+
+@RestController
+@CrossOrigin //Needed for the Frontend
+@RequestMapping("cards") //Pre sets the URI with cards
+public class CardController {
+    @Autowired
+    CardService cardService;
+
+    @GetMapping
+    public ResponseEntity<List<CardResponse>> getCards() { //Response Entity to return the HTTP Status
+        return ResponseEntity.ok(cardService.getCards()); //Ok == Status code 200
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<CardResponse> getCard(@PathVariable int id) { //PathVariable to get the id in the URL
+        return ResponseEntity.ok(cardService.getCard(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<CardResponse> saveCard(@RequestBody CardRequest request){
+        CardResponse response = cardService.saveCard(request); //Save and the get the element with ID
+        
+        //Creating the URI of the new element
+        URI location = ServletUriComponentsBuilder //Class to create URI
+                       .fromCurrentRequest() //Get the current URI
+                       .path("/{id}") //Add /{id} at the and of the URI
+                       .buildAndExpand(response.id()) //Replace {id} with the id of the element
+                       .toUri(); //Create the new URI
+
+        //Returning the response entity with new element
+        return ResponseEntity.created(location).body(response); //Created == 201
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> updateCard(@PathVariable int id, @RequestBody CardRequest request){
+        cardService.updateCard(id, request);
+        return ResponseEntity.ok().build(); //Return the status code 200, with no content
+    }
+}
