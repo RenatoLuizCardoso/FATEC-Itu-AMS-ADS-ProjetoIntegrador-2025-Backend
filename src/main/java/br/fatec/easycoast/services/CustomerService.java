703617f5f51329ec395c075566009bb268322463
YuuyaOkatani <yuuyaokatani@gmail.com>
2025-05-08 16:09:32 -0300

    :sparkles: feat: Updated User to Employee, Employee Entity, DTOs, Service and Controller

--- src/main/java/br/fatec/easycoast/services/CustomerService.java
+++ src/main/java/br/fatec/easycoast/services/CustomerService.java
@@ -1,7 +1,6 @@
 package br.fatec.easycoast.services;
 
 import java.util.List;
-import java.util.Locale.Category;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
