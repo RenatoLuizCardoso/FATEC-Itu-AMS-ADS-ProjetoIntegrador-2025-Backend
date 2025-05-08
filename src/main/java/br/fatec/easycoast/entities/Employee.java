3c4261f8e1ce22aaac5528285fee133fdeb1bd7f
YuuyaOkatani <yuuyaokatani@gmail.com>
2025-05-08 16:12:50 -0300

    :sparkles: feat: fixed SQL

--- src/main/java/br/fatec/easycoast/entities/Employee.java
+++ src/main/java/br/fatec/easycoast/entities/Employee.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "TBL_USERS")
+@Table(name = "TBL_EMPLOYEE")
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
